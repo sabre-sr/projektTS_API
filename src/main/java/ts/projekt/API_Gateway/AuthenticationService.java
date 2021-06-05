07692a05514d0174379040a53995833d0053cf80
sabre <djsoundnr1@gmail.com>
2021-06-05 12:48:46 +0200

    getting salt works

--- src/main/java/ts/projekt/API_Gateway/AuthenticationService.java
+++ src/main/java/ts/projekt/API_Gateway/AuthenticationService.java
@@ -1,10 +1,26 @@
 package ts.projekt.API_Gateway;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class AuthenticationService {
+    //test salt: F2 40 20 FA DE 76 39 02 4A D8 BB 0E B6 7E AC 22
     @GetMapping(path = "getsalt/{id}")
-    public byte[]
+    public AuthUser getSalt(@PathVariable int id) {
+        String uri = "http://localhost:8083/getsalt/" + id;
+        WebClient salt = WebClient.builder()
+                .baseUrl(uri)
+                .build();
+        return salt.get()
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<AuthUser>() {
+                })
+                .block();
+    }
 }
