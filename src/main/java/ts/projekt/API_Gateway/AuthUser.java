5af9096cd1c4b468cac3ffb5681386355aafce29
sabre <djsoundnr1@gmail.com>
2021-06-05 12:21:57 +0200

    starting work on Auth API

---
+++ src/main/java/ts/projekt/API_Gateway/AuthUser.java
@@ -0,0 +1,38 @@
+package ts.projekt.API_Gateway;
+
+import java.io.Serializable;
+
+public class AuthUser extends User implements Serializable {
+    private String hash;
+    private byte[] salt;
+
+    public AuthUser(){
+        super();
+    }
+
+    public AuthUser(int id) {
+        super(id);
+    }
+
+    public AuthUser(int id, String hash, byte[] salt) {
+        super(id);
+        this.hash = hash;
+        this.salt = salt;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+}
--- src/main/java/ts/projekt/API_Gateway/AuthenticationService.java
+++ src/main/java/ts/projekt/API_Gateway/AuthenticationService.java
@@ -1,7 +1,10 @@
 package ts.projekt.API_Gateway;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AuthenticationService {
-
+    @GetMapping(path = "getsalt/{id}")
+    public byte[]
 }
